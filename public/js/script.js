dce587f2a7f24ba3204f6fd2641d64badea5fa6c
sevro49 <21120205303@ismu.edu.tr>
2023-06-18 00:27:32 +0300

    changed access method

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,11 @@
 const username = "sevro49";
-let token = "";
-const filePath = '../../access_token.txt';
+const projectsElement = document.querySelector(".profile div:nth-child(2) .number-of-repos");
 
-// Check if running on GitHub Pages or locally
-if (window.location.hostname === "sevro49.github.io") {
-  // Running on GitHub Pages, use environment variable
-  token = process.env.ACCESS_TOKEN;
-} else {
-  // Running locally, use fallback token value
-  const projectsElement = document.querySelector(".profile div:nth-child(2) .number-of-repos");
-
-  fetch(filePath)
-    .then(response => response.text())
-    .then(data => {
-        fetch(`https://api.github.com/users/${username}`, {})
-            .then(response => response.json())
-            .then(data => {
-                const numRepos = data.public_repos;
-                // Display the number of repositories on your website
-                projectsElement.innerText = `${numRepos}`;
-                console.log(numRepos);
-            })
-            .catch(error => console.error(error));
+fetch(`https://api.github.com/users/${username}`, {})
+   .then(response => response.json())
+   .then(data => {
+       const numRepos = data.public_repos;
+        projectsElement.innerText = `${numRepos}`;
     })
-    .catch(error => {
-        console.error('File could not be read', error);
-    });
-}
-
-
-
+    .catch(error => console.error(error));
 
