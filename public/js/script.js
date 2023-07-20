61831b6a2286baf4455d526cb613f66ff179570d
Emre Güler <21120205303@ismu.edu.tr>
2023-07-20 15:41:36 +0000

    updated Profile section inner text

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,13 @@
 const username = "sevro49";
 const projectsElement = document.querySelector(".profile div:nth-child(2) .number-of-repos");
+const projectsElementText = document.querySelector("#home__b .hours");
 
 fetch(`https://api.github.com/users/${username}`, {})
    .then(response => response.json())
    .then(data => {
        const numRepos = data.public_repos;
         projectsElement.innerText = `${numRepos}`;
+        projectsElementText.innerText = `${numRepos}`;
     })
     .catch(error => console.error(error));
 
